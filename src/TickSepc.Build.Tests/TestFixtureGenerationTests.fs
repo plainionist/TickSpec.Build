a6c70f6de30da4006022e95e5370d461ce855d48
plainionist <mail@plainionist.net>
2023-03-26 17:22:09 +0200

    more test cases added

--- src/TickSepc.Build.Tests/TestFixtureGenerationTests.fs
+++ src/TickSepc.Build.Tests/TestFixtureGenerationTests.fs
@@ -4,7 +4,6 @@ open NUnit.Framework
 open TickSpec.Build
 open FsUnit
 
-
 [<Test>]
 let ``Single scenario``() =
     [
@@ -12,6 +11,58 @@ let ``Single scenario``() =
         Feature: First feature
 
         Scenario: One
+        GIVEN some environment
+        WHEN some event happens
+        THEN the system should be in this state
+        """
+    ]
+    |> TestApi.GenerateTestFixtures
+    |> should haveSubstringIgnoringWhitespaces  """
+        namespace Specification
+
+        open System.Reflection
+        open NUnit.Framework
+        open TickSpec.CodeGen
+
+        [<TestFixture>]
+        type ``First feature``() = 
+            inherit AbstractFeature()
+
+            let scenarios = AbstractFeature.GetScenarios(Assembly.GetExecutingAssembly(), "Dummy.feature")
+
+            [<Test>]
+            member this.``One``() =
+        #line 5 "Dummy.feature"
+                this.RunScenario(scenarios, "Scenario: One")
+            """
+
+
+[<Test>]
+let ``Multiple features with multipe scenario``() =
+    [
+        """
+        Feature: First feature
+
+        Scenario: One
+        GIVEN some environment
+        WHEN some event happens
+        THEN the system should be in this state
+
+        Scenario: Two
+        GIVEN some environment
+        WHEN some event happens
+        THEN the system should be in this state
+        """
+
+        """
+        Feature: Second feature
+
+        Scenario: Three
+        GIVEN some environment
+        WHEN some event happens
+        THEN the system should be in this state
+
+        Scenario: Four
         GIVEN some environment
         WHEN some event happens
         THEN the system should be in this state
@@ -36,5 +87,26 @@ let ``Single scenario``() =
             member this.``One``() =
         #line 5 "Dummy.feature"
                 this.RunScenario(scenarios, "Scenario: One")
+
+            [<Test>]
+            member this.``Two``() =
+        #line 10 "Dummy.feature"
+                this.RunScenario(scenarios, "Scenario: Two")
+
+        [<TestFixture>]
+        type ``Second feature``() = 
+            inherit AbstractFeature()
+
+            let scenarios = AbstractFeature.GetScenarios(Assembly.GetExecutingAssembly(), "Dummy.feature")
+
+            [<Test>]
+            member this.``Three``() =
+        #line 5 "Dummy.feature"
+                this.RunScenario(scenarios, "Scenario: Three")
+
+            [<Test>]
+            member this.``Four``() =
+        #line 10 "Dummy.feature"
+                this.RunScenario(scenarios, "Scenario: Four")
             """
 
